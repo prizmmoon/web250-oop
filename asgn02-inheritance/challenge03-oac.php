692aafb8200ac4ee76a72e18d7a6d9d68cd6c281
Prizm Moon <prdigitalmoon@gmail.com>
2024-09-03 16:35:28 -0400

    Adds bicycle instances and code to display them

--- asgn02-inheritance/challenge03-oac.php
+++ asgn02-inheritance/challenge03-oac.php
@@ -16,7 +16,7 @@ class Bicycle
 
   public function wheel_details()
   {
-    return "It has {$this->wheels} wheels";
+    return "It has {$this->wheels} wheels. <br>";
   }
 
   public function set_weight_kg($value)
@@ -45,3 +45,36 @@ class Unicycle extends Bicycle
 {
   protected $wheels = 1;
 }
+
+$cannondale = new Bicycle;
+$cannondale->brand = "Cannondale";
+$cannondale->model = "Trail";
+$cannondale->year = "2020";
+$cannondale->set_weight_kg(13);
+
+$fuji = new Bicycle;
+$fuji->brand = "Fuji";
+$fuji->model = "Kasane";
+$fuji->year = "2022";
+$fuji->set_weight_kg(9);
+
+$nimbus = new Unicycle;
+$nimbus->brand = "Nimbus";
+$nimbus->model = "Hatchet";
+$nimbus->year = "2022";
+$nimbus->set_weight_kg(16);
+
+echo $cannondale->name() . "<br>";
+echo $cannondale->get_weight_kg() . "<br>";
+echo $cannondale->weight_lbs() . "<br>";
+echo $cannondale->wheel_details() . "<br>";
+
+echo $fuji->name() . "<br>";
+echo $fuji->get_weight_kg() . "<br>";
+echo $fuji->weight_lbs() . "<br>";
+echo $fuji->wheel_details() . "<br>";
+
+echo $nimbus->name() . "<br>";
+echo $nimbus->get_weight_kg() . "<br>";
+echo $nimbus->weight_lbs() . "<br>";
+echo $nimbus->wheel_details() . "<br>";
