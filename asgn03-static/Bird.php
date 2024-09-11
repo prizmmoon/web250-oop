786a9ef13732ba98d762feec62a9faa7f06ec0e5
Prizm Moon <prdigitalmoon@gmail.com>
2024-09-10 23:39:27 -0400

    Adds access modifiers, properties and methods

--- asgn03-static/Bird.php
+++ asgn03-static/Bird.php
@@ -1,31 +1,45 @@
 <?php
 
-class Bird {
-    var $habitat;
-    var $food;
-    var $nesting = "tree";
-    var $conservation;
-    var $song = "chirp";
-    var $flying = "yes";
+class Bird
+{
+    public static $instanceCount = 0;
 
-    function can_fly() {
-        if ( $this->flying == "yes" ) {
-            $flying_string = "can fly";
-        } else {
-            $flying_string = "is stuck on the ground";
-        }
-        return  $flying_string ;
+    protected $habitat;
+    protected $food;
+    protected static $nesting = "tree";
+    protected $conservation;
+    protected static $song = "chirp";
+    protected static $flying = "yes";
+    protected static $eggNum = "one massive egg";
+
+    protected function canFly()
+    {
+        $flyingString = static::$flying == "yes" ? " bird can fly" : "cannot fly and is stuck on the ground";
+
+        return $flyingString;
+    }
+
+    public static function create()
+    {
+        $className = get_called_class();
+        $object = new $className;
+
+        static::$instanceCount++;
+        return $object;
     }
 }
 
-class YellowBelliedFlyCatcher extends Bird {
-    var $name = "yellow-bellied flycatcher";
-    var $diet = "mostly insects.";
-    var $song = "flat chilk";
+class YellowBelliedFlyCatcher extends Bird
+{
+    private $name = "yellow-bellied flycatcher";
+    private $diet = "mostly insects.";
+    protected static $song = "flat chilk";
+    protected static $eggNum = "3-4, sometimes 5";
 }
 
-class Kiwi extends Bird {
-    var $name = "kiwi";
-    var $diet = "omnivorous";
-    var $flying = "no";
+class Kiwi extends Bird
+{
+    private $name = "kiwi";
+    private $diet = "omnivorous";
+    protected static $flying = "no";
 }
