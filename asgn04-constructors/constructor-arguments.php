33ebd66addcb8493a94a3b7265167e000c5e58ce
Prizm Moon <prdigitalmoon@gmail.com>
2024-09-16 21:52:38 -0400

    Instantiates birds

--- asgn04-constructors/constructor-arguments.php
+++ asgn04-constructors/constructor-arguments.php
@@ -7,7 +7,25 @@ class Bird
 
   public function __construct($args = [])
   {
-    $this->latinName = $args["latin name"] ?? NULL;
     $this->commonName = $args["common name"] ?? NULL;
+    $this->latinName = $args["latin name"] ?? NULL;
   }
 }
+
+$flycatcher = new Bird([
+  "common name" => "Acadian Flycatcher",
+  "latin name" => "Empidonax virescens"
+]);
+
+$wren = new Bird([
+  "common name" => "Carolina Wren",
+  "latin name" => "Thyrothorus ludovicianus"
+]);
+
+echo "Common name: " . $flycatcher->$commonName . "<br>";
+echo "Latin name: " . $flycatcher->$latinName . "<br>";
+
+echo "<hr>";
+
+echo "Common name: " . $wren->$commonName . "<br>";
+echo "Latin name: " . $wren->$latinName . "<br>";
