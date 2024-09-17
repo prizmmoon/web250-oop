6f5484d3ccf03684c19d79b24218846d8b40d780
Prizm Moon <prdigitalmoon@gmail.com>
2024-09-16 21:21:37 -0400

    Instantiates birds

--- asgn04-constructors/constructor.php
+++ asgn04-constructors/constructor.php
@@ -5,5 +5,21 @@ class Bird
   public $commonName;
   public $latinName;
 
-  public function __constructor($commonName, $latinName) {}
+  public function __construct($commonName, $latinName)
+  {
+    $this->commonName = $commonName;
+    $this->latinName = $latinName;
+  }
 }
+
+$robin = new Bird("American Robin", "Turdus migratorius");
+
+$towhee = new Bird("Eastern Towhee", "Pipilo erythrophthalmus");
+
+echo "Common name: " . $robin->commonName . "<br>";
+echo "Latin name: " . $robin->latinName . "<br>";
+
+echo "<hr>";
+
+echo "Common name: " . $towhee->commonName . "<br>";
+echo "Latin name: " . $towhee->latinName . "<br>";
