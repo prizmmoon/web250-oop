f4ffa632a7b23af19dda5ffcdf61392eeaa9e48c
Prizm Moon <prdigitalmoon@gmail.com>
2024-09-24 03:55:41 -0400

    Adds functions

--- chain_gang/private/classes/bicycle.class.php
+++ chain_gang/private/classes/bicycle.class.php
@@ -56,4 +56,34 @@ class Bicycle
       }
     }
   }
+
+  public function weight_kg()
+  {
+    return number_format($this->weight_kg, 2) . "kg";
+  }
+
+  public function set_weight_kg($value)
+  {
+    $this->weight_kg = floatval($value);
+  }
+
+  public function weight_lbs()
+  {
+    $weight_lbs = floatval($this->weight_kg) * 2.2046226218;
+    return number_format($weight_lbs, 2) . " lbs";
+  }
+
+  public function set_weight_lbs($value)
+  {
+    $this->weight_kg = floatval($value) / 2.2046226218;
+  }
+
+  public function condition()
+  {
+    if ($this->condition_id > 0) {
+      return self::CONDITION_OPTIONS[$this->condition_id];
+    } else {
+      return "Unknown";
+    }
+  }
 }
