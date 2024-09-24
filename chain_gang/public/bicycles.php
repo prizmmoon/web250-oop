f951a10b81dcb8611aa99323e247d68496c21252
Prizm Moon <prdigitalmoon@gmail.com>
2024-09-24 04:35:54 -0400

    Creates object to update rows with variables

--- chain_gang/public/bicycles.php
+++ chain_gang/public/bicycles.php
@@ -26,16 +26,23 @@
         <th>Price</th>
       </tr>
 
+      <?php
+
+      $args = ["brand" => "Trek", "model" => "Emonda", "year" => 2017];
+      $bike = new Bicycle;
+
+      ?>
+
       <tr>
-        <td>Brand</td>
-        <td>Model</td>
-        <td>Year</td>
-        <td>Category</td>
-        <td>Gender</td>
-        <td>Color</td>
-        <td>Weight</td>
-        <td>Condition</td>
-        <td>Price</td>
+        <td><?php echo h($bike->brand); ?></td>
+        <td><?php echo h($bike->model); ?></td>
+        <td><?php echo h($bike->year); ?></td>
+        <td><?php echo h($bike->category); ?></td>
+        <td><?php echo h($bike->gender); ?></td>
+        <td><?php echo h($bike->color); ?></td>
+        <td><?php echo h($bike->weight_kg()) . "/" . h($bike->weight_lbs()); ?></td>
+        <td><?php echo h($bike->condition()); ?></td>
+        <td><?php echo h("$" . $bike->price); ?></td>
       </tr>
 
     </table>
