fb1b3ff4439997a63ce6288ee02ad5fd910a6b9b
Prizm Moon <prdigitalmoon@gmail.com>
2024-11-20 00:45:03 -0500

    Makes Bird inherit properly from DatabaseObject

--- asgn09-delete/private/classes/DatabaseObject.class.php
+++ asgn09-delete/private/classes/DatabaseObject.class.php
@@ -1,25 +1,29 @@
 <?php
 
-class DatabaseObject {
+class DatabaseObject
+{
 
   static protected $database;
   static protected $table_name = "";
   static protected $columns = [];
   public $errors = [];
 
-  static public function set_database($database) {
+  static public function set_database($database)
+  {
     self::$database = $database;
   }
 
-  static public function find_by_sql($sql) {
+  static public function find_by_sql($sql)
+  {
     $result = self::$database->query($sql);
-    if(!$result) {
+
+    if (!$result) {
       exit("Database query failed.");
     }
 
     // results into objects
     $object_array = [];
-    while($record = $result->fetch_assoc()) {
+    while ($record = $result->fetch_assoc()) {
       $object_array[] = static::instantiate($record);
     }
 
@@ -28,127 +32,144 @@ class DatabaseObject {
     return $object_array;
   }
 
-  static public function find_all() {
-    $sql = "SELECT * FROM " . static::$table_name;
-    return static::find_by_sql($sql);
+  static public function find_all()
+  {
+    $sql = "SELECT * FROM " . static::$table_name . " ";
+    $result = static::$database->query($sql);
+
+    $birds = [];
+    while ($row = $result->fetch_object()) {
+      $birds[] = $row;
+    }
+
+    return $birds;
   }
 
-  static public function find_by_id($id) {
-    $sql = "SELECT * FROM " . static::$table_name . " ";
+  static public function find_by_id($id)
+  {
+    $sql = "SELECT * FROM birds " . static::$table_name . " ";
     $sql .= "WHERE id='" . self::$database->escape_string($id) . "'";
     $obj_array = static::find_by_sql($sql);
-    if(!empty($obj_array)) {
+    if (!empty($obj_array)) {
       return array_shift($obj_array);
     } else {
       return false;
     }
   }
 
-  static protected function instantiate($record) {
+  static protected function instantiate($record)
+  {
     $object = new static;
-    // Could manually assign values to properties
-    // but automatically assignment is easier and re-usable
-    foreach($record as $property => $value) {
-      if(property_exists($object, $property)) {
+    // Could automatically assign values to properties
+    // but automatic assignment is easier and re-usable
+    foreach ($record as $property => $value) {
+      if (property_exists($object, $property)) {
         $object->$property = $value;
       }
     }
+
     return $object;
   }
 
-  protected function validate() {
+  protected function validate()
+  {
     $this->errors = [];
-
-    // Add custom validations
 
     return $this->errors;
   }
 
-  protected function create() {
+  protected function create()
+  {
     $this->validate();
-    if(!empty($this->errors)) { return false; }
+    if (!empty($this->errors)) {
+      return false;
+    }
 
     $attributes = $this->sanitized_attributes();
-    $sql = "INSERT INTO " . static::$table_name . " (";
+
+    $sql = "INSERT INTO birds " . static::$table_name . "( ";
     $sql .= join(', ', array_keys($attributes));
     $sql .= ") VALUES ('";
     $sql .= join("', '", array_values($attributes));
     $sql .= "')";
+
     $result = self::$database->query($sql);
-    if($result) {
+    if ($result) {
       $this->id = self::$database->insert_id;
     }
     return $result;
   }
 
-  protected function update() {
+  protected function update()
+  {
     $this->validate();
-    if(!empty($this->errors)) { return false; }
+    if (!empty($this->errors)) {
+      return false;
+    }
 
     $attributes = $this->sanitized_attributes();
     $attribute_pairs = [];
-    foreach($attributes as $key => $value) {
+    foreach ($attributes as $key => $value) {
       $attribute_pairs[] = "{$key}='{$value}'";
     }
 
-    $sql = "UPDATE " . static::$table_name . " SET ";
+    $sql = "UPDATE birds SET " . static::$table_name . " ";
     $sql .= join(', ', $attribute_pairs);
     $sql .= " WHERE id='" . self::$database->escape_string($this->id) . "' ";
-    $sql .= "LIMIT 1";
+
     $result = self::$database->query($sql);
     return $result;
   }
 
-  public function save() {
-    // A new record will not have an ID yet
-    if(isset($this->id)) {
+  public function save()
+  {
+    // A new record will not have an id yet
+    if (isset($this->id)) {
       return $this->update();
     } else {
       return $this->create();
     }
   }
 
-  public function merge_attributes($args=[]) {
-    foreach($args as $key => $value) {
-      if(property_exists($this, $key) && !is_null($value)) {
+  public function merge_attributes($args = [])
+  {
+    foreach ($args as $key => $value) {
+      if (property_exists($this, $key) && !is_null($value)) {
         $this->$key = $value;
       }
     }
   }
 
-  // Properties which have database columns, excluding ID
-  public function attributes() {
+  public function attributes()
+  {
     $attributes = [];
-    foreach(static::$db_columns as $column) {
-      if($column == 'id') { continue; }
+    foreach (static::$db_columns as $column) {
+      if ($column == 'id') {
+        continue;
+      }
+
       $attributes[$column] = $this->$column;
     }
+
     return $attributes;
   }
 
-  protected function sanitized_attributes() {
+  protected function sanitized_attributes()
+  {
     $sanitized = [];
-    foreach($this->attributes() as $key => $value) {
+    foreach ($this->attributes() as $key => $value) {
       $sanitized[$key] = self::$database->escape_string($value);
     }
+
     return $sanitized;
   }
 
-  public function delete() {
-    $sql = "DELETE FROM " . static::$table_name . " ";
-    $sql .= "WHERE id='" . self::$database->escape_string($this->id) . "' ";
+  public function delete()
+  {
+    $sql = "DELETE FROM birds " . static::$table_name . " ";
+    $sql .= "WHERE ID='" . self::$database->escape_string($this->id) . "' ";
     $sql .= "LIMIT 1";
     $result = self::$database->query($sql);
     return $result;
-
-    // After deleting, the instance of the object will still
-    // exist, even though the database record does not.
-    // This can be useful, as in:
-    //   echo $user->first_name . " was deleted.";
-    // but, for example, we can't call $user->update() after
-    // calling $user->delete().
   }
-
 }
-
-?>
--- asgn09-delete/private/classes/bird.class.php
+++ asgn09-delete/private/classes/bird.class.php
@@ -7,180 +7,6 @@ class Bird extends DatabaseObject
   static protected $db_columns = ['id', 'common_name', 'habitat', 'food', 'conservation_id', 'backyard_tips'];
   public $errors = [];
 
-  // ------------ START OF ACTIVE RECORD CODE ------------
-  static protected $database;
-
-  static public function set_database($database)
-  {
-    self::$database = $database;
-  }
-
-  static public function find_by_sql($sql)
-  {
-    $result = self::$database->query($sql);
-
-    if (!$result) {
-      exit("Database query failed.");
-    }
-
-    // results into objects
-    $object_array = [];
-    while ($record = $result->fetch_assoc()) {
-      $object_array[] = self::instantiate($record);
-    }
-
-    $result->free();
-
-    return $object_array;
-  }
-
-  static public function find_all()
-  {
-    $sql = "SELECT * FROM birds";
-    $result = self::$database->query($sql);
-
-    $birds = [];
-    while ($row = $result->fetch_object()) {
-      $birds[] = $row;
-    }
-
-    return $birds;
-  }
-
-  static public function find_by_id($id)
-  {
-    $sql = "SELECT * FROM birds ";
-    $sql .= "WHERE id='" . self::$database->escape_string($id) . "'";
-    $obj_array = self::find_by_sql($sql);
-    if (!empty($obj_array)) {
-      return array_shift($obj_array);
-    } else {
-      return false;
-    }
-  }
-
-  static protected function instantiate($record)
-  {
-    $object = new self;
-    // Could automatically assign values to properties
-    // but automatic assignment is easier and re-usable
-    foreach ($record as $property => $value) {
-      if (property_exists($object, $property)) {
-        $object->$property = $value;
-      }
-    }
-
-    return $object;
-  }
-
-  protected function validate()
-  {
-    $this->errors = [];
-
-    if (is_blank($this->common_name)) {
-      $this->errors[] = "Bird name cannot be blank.";
-    }
-
-    return $this->errors;
-  }
-
-  protected function create()
-  {
-    $this->validate();
-    if (!empty($this->errors)) {
-      return false;
-    }
-
-    $attributes = $this->sanitized_attributes();
-
-    $sql = "INSERT INTO birds (";
-    $sql .= join(', ', array_keys($attributes));
-    $sql .= ") VALUES ('";
-    $sql .= join("', '", array_values($attributes));
-    $sql .= "')";
-
-    $result = self::$database->query($sql);
-    if ($result) {
-      $this->id = self::$database->insert_id;
-    }
-    return $result;
-  }
-
-  protected function update()
-  {
-    $this->validate();
-    if (!empty($this->errors)) {
-      return false;
-    }
-
-    $attributes = $this->sanitized_attributes();
-    $attribute_pairs = [];
-    foreach ($attributes as $key => $value) {
-      $attribute_pairs[] = "{$key}='{$value}'";
-    }
-
-    $sql = "UPDATE birds SET ";
-    $sql .= join(', ', $attribute_pairs);
-    $sql .= " WHERE id='" . self::$database->escape_string($this->id) . "' ";
-
-    $result = self::$database->query($sql);
-    return $result;
-  }
-
-  public function save()
-  {
-    // A new record will not have an id yet
-    if (isset($this->id)) {
-      return $this->update();
-    } else {
-      return $this->create();
-    }
-  }
-
-  public function merge_attributes($args = [])
-  {
-    foreach ($args as $key => $value) {
-      if (property_exists($this, $key) && !is_null($value)) {
-        $this->$key = $value;
-      }
-    }
-  }
-
-  public function attributes()
-  {
-    $attributes = [];
-    foreach (self::$db_columns as $column) {
-      if ($column == 'id') {
-        continue;
-      }
-
-      $attributes[$column] = $this->$column;
-    }
-
-    return $attributes;
-  }
-
-  protected function sanitized_attributes()
-  {
-    $sanitized = [];
-    foreach ($this->attributes() as $key => $value) {
-      $sanitized[$key] = self::$database->escape_string($value);
-    }
-
-    return $sanitized;
-  }
-
-  public function delete()
-  {
-    $sql = "DELETE FROM birds ";
-    $sql .= "WHERE ID='" . self::$database->escape_string($this->id) . "' ";
-    $sql .= "LIMIT 1";
-    $result = self::$database->query($sql);
-    return $result;
-  }
-
-  // ------------ END OF ACTIVE RECORD CODE ------------  
-
   public $id;
   public $common_name;
   public $habitat;
@@ -214,5 +40,16 @@ class Bird extends DatabaseObject
     } else {
       return "Unknown";
     }
+  }
+
+  protected function validate()
+  {
+    $this->errors = [];
+
+    if (is_blank($this->common_name)) {
+      $this->errors[] = "Bird name cannot be blank.";
+    }
+
+    return $this->errors;
   }
 }
