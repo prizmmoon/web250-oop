0aeae0877dc13f9f76378986fc691413df94413a
Prizm Moon <prdigitalmoon@gmail.com>
2024-12-18 00:56:28 -0500

    Adds new php code

--- asgn11-three-views/public/users/new.php
+++ asgn11-three-views/public/users/new.php
@@ -1,12 +1,52 @@
 <?php
 
 require_once('../../../private/initialize.php');
+require_login();
 
-/* 
-  Use the bicycles/staff/new.php file as a guide 
-  so your file mimics the same functionality.
-  Be sure to include the display_errors() function.
-*/
+if (is_post_request()) {
 
+  // Create record using post parameters
+  $args = $_POST['user'];
+  $user = new User($args);
+  $result = $user->save();
 
-include(SHARED_PATH . '/public_footer.php');
+  if ($result === true) {
+    $new_id = $user->id;
+    $session->message('The user was created successfully.');
+    redirect_to(url_for('/users/show.php?id=' . $new_id));
+  } else {
+    // show errors
+  }
+} else {
+  // display the form
+  $user = new User;
+}
+
+?>
+
+<?php $page_title = 'Create User'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+
+  <a class="back-link" href="<?php echo url_for('/users/index.php'); ?>">&laquo; Back to List</a>
+
+  <div class="user new">
+    <h1>Create User</h1>
+
+    <?php echo display_errors($user->errors); ?>
+
+    <form action="<?php echo url_for('/users/new.php'); ?>" method="post">
+
+      <?php include('form_fields.php'); ?>
+
+      <div id="operations">
+        <input type="submit" value="Create User" />
+      </div>
+    </form>
+
+  </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
