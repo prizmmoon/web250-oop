d0d29b3038822467105e2fe848d52bc2acb6f0f2
Prizm Moon <prdigitalmoon@gmail.com>
2024-12-18 00:16:43 -0500

    Adds User class code

--- asgn11-three-views/private/classes/User.class.php
+++ asgn11-three-views/private/classes/User.class.php
@@ -3,53 +3,127 @@
 class User extends DatabaseObject
 {
 
-  static protected $table_name = 'users';
-  static protected $db_columns = ['id', 'common_name', 'habitat', 'food', 'conservation_id', 'backyard_tips'];
-  public $errors = [];
+  static protected $table_name = "users";
+  static protected $db_columns = ['id', 'first_name', 'last_name', 'email', 'username', 'hashed_password'];
 
   public $id;
-  public $common_name;
-  public $habitat;
-  public $food;
-  public $conservation_id;
-  public $backyard_tips;
-
-  protected const CONSERVATION_OPTIONS = [
-    1 => 'Low concern',
-    2 => 'Moderate concern',
-    3 => 'Extreme concern',
-    4 => 'Extinct',
-  ];
-
+  public $first_name;
+  public $last_name;
+  public $email;
+  public $username;
+  protected $hashed_password;
+  public $password;
+  public $confirm_password;
+  protected $password_required = true;
 
   public function __construct($args = [])
   {
-    $this->common_name = $args['common_name'] ?? '';
-    $this->habitat = $args['habitat'] ?? '';
-    $this->food = $args['food'] ?? '';
-    $this->behavior = $args['behavior'] ?? '';
-    $this->conservation_id = $args['conservation_id'] ?? '';
-    $this->backyard_tips = $args['backyard_tips'] ?? '';
+    $this->first_name = $args['first_name'] ?? '';
+    $this->last_name = $args['last_name'] ?? '';
+    $this->email = $args['email'] ?? '';
+    $this->username = $args['username'] ?? '';
+    $this->password = $args['password'] ?? '';
+    $this->confirm_password = $args['confirm_password'] ?? '';
   }
 
-
-  public function conservation()
+  public function full_name()
   {
-    if ($this->conservation_id > 0) {
-      return self::CONSERVATION_OPTIONS[$this->conservation_id];
+    return $this->first_name . " " . $this->last_name;
+  }
+
+  protected function set_hashed_password()
+  {
+    $this->hashed_password = password_hash($this->password, PASSWORD_BCRYPT);
+  }
+
+  public function verify_password($password)
+  {
+    return password_verify($password, $this->hashed_password);
+  }
+
+  protected function create()
+  {
+    $this->set_hashed_password();
+    return parent::create();
+  }
+
+  protected function update()
+  {
+    if ($this->password != '') {
+      $this->set_hashed_password();
     } else {
-      return "Unknown";
+      $this->password_required = false;
     }
+
+    return parent::create();
   }
 
   protected function validate()
   {
     $this->errors = [];
 
-    if (is_blank($this->common_name)) {
-      $this->errors[] = "Bird name cannot be blank.";
+    if (is_blank($this->first_name)) {
+      $this->errors[] = "First name cannot be blank.";
+    } elseif (!has_length($this->first_name, array('min' => 2, 'max' => 255))) {
+      $this->errors[] = "First name must be between 2 and 255 characters.";
+    }
+
+    if (is_blank($this->last_name)) {
+      $this->errors[] = "Last name cannot be blank.";
+    } elseif (!has_length($this->last_name, array('min' => 2, 'max' => 255))) {
+      $this->errors[] = "Last name must be between 2 and 255 characters.";
+    }
+
+    if (is_blank($this->email)) {
+      $this->errors[] = "Email cannot be blank.";
+    } elseif (!has_length($this->email, array('max' => 255))) {
+      $this->errors[] = "Last name must be less than 255 characters.";
+    } elseif (!has_valid_email_format($this->email)) {
+      $this->errors[] = "Email must be a valid format.";
+    }
+
+    if (is_blank($this->username)) {
+      $this->errors[] = "Username cannot be blank.";
+    } elseif (!has_length($this->username, array('min' => 8, 'max' => 255))) {
+      $this->errors[] = "Username must be between 8 and 255 characters.";
+    } elseif (!has_unique_username($this->username, $this->id ?? 0)) {
+      $this->errors[] = "Username not allowed. Try another.";
+    }
+
+    if ($this->password_required) {
+      if (is_blank($this->password)) {
+        $this->errors[] = "Password cannot be blank.";
+      } elseif (!has_length($this->password, array('min' => 12))) {
+        $this->errors[] = "Password must contain 12 or more characters";
+      } elseif (!preg_match('/[A-Z]/', $this->password)) {
+        $this->errors[] = "Password must contain at least 1 uppercase letter";
+      } elseif (!preg_match('/[a-z]/', $this->password)) {
+        $this->errors[] = "Password must contain at least 1 lowercase letter";
+      } elseif (!preg_match('/[0-9]/', $this->password)) {
+        $this->errors[] = "Password must contain at least 1 number";
+      } elseif (!preg_match('/[^A-Za-z0-9\s]/', $this->password)) {
+        $this->errors[] = "Password must contain at least 1 symbol";
+      }
+
+      if (is_blank($this->confirm_password)) {
+        $this->errors[] = "Confirm password cannot be blank.";
+      } elseif ($this->password !== $this->confirm_password) {
+        $this->errors[] = "Password and confirm password must match.";
+      }
     }
 
     return $this->errors;
+  }
+
+  static public function find_by_username($username)
+  {
+    $sql = "SELECT * FROM " . static::$table_name . " ";
+    $sql .= "WHERE username='" . self::$database->escape_string($username) . "'";
+    $obj_array = static::find_by_sql($sql);
+    if (!empty($obj_array)) {
+      return array_shift($obj_array);
+    } else {
+      return false;
+    }
   }
 }
