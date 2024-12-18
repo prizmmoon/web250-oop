691f9ed42144ad4bb89b462f473e74735f518145
Prizm Moon <prdigitalmoon@gmail.com>
2024-12-18 00:47:00 -0500

    Adds edit code

--- asgn11-three-views/public/users/edit.php
+++ asgn11-three-views/public/users/edit.php
@@ -2,15 +2,61 @@
 
 require_once('../../../private/initialize.php');
 
-/* 
-  Use the bicycles/staff/edit.php file as a guide 
-  so your file mimics the same functionality.
-  Be sure to include the display_errors() function.
-*/
+require_login();
 
-$page_title = 'Edit User';
-include(SHARED_PATH . '/public_header.php');
+if (!isset($_GET['id'])) {
+  redirect_to(url_for('/users/index.php'));
+}
+$id = $_GET['id'];
+$user = User::find_by_id($id);
+if ($user == false) {
+  redirect_to(url_for('/users/index.php'));
+}
 
+if (is_post_request()) {
 
+  // Save record using post parameters
+  $args = $_POST['user'];
+  $user->merge_attributes($args);
+  $result = $user->save();
 
-include(SHARED_PATH . '/public_footer.php');
+  if ($result === true) {
+    $session->message('The user was updated successfully.');
+    redirect_to(url_for('/users/show.php?id=' . $id));
+  } else {
+    // show errors
+  }
+} else {
+
+  // display the form
+
+}
+
+?>
+
+<?php $page_title = 'Edit User'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+
+  <a class="back-link" href="<?php echo url_for('/users/index.php'); ?>">&laquo; Back to List</a>
+
+  <div class="user edit">
+    <h1>Edit User</h1>
+
+    <?php echo display_errors($user->errors); ?>
+
+    <form action="<?php echo url_for('/users/edit.php?id=' . h(u($id))); ?>" method="post">
+
+      <?php include('form_fields.php'); ?>
+
+      <div id="operations">
+        <input type="submit" value="Edit User" />
+      </div>
+    </form>
+
+  </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
