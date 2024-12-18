aef475dcf3a4704862fda4c3e7abc612757a5117
Prizm Moon <prdigitalmoon@gmail.com>
2024-12-18 02:57:08 -0500

    Adds has_unique_username code

--- asgn11-three-views/private/validation_functions.php
+++ asgn11-three-views/private/validation_functions.php
@@ -110,5 +110,12 @@ function has_valid_email_format($value)
 //   has_unique_username('johnqpublic', 4)
 function has_unique_username($username, $current_id = "0")
 {
-  // Need to re-write for OOP
+  $user = User::find_by_username($username);
+  if ($user === false || $user->id == $current_id) {
+    // is unique
+    return true;
+  } else {
+    // not unique
+    return false;
+  }
 }
